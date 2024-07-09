c82a415a2ab1b7e8daa206584a24b6d8d8182c65
Anastasia <nassstya442@gmail.com>
2024-07-09 18:34:19 +0300

    Initial commit

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -12,39 +12,6 @@ class TelegramController extends Controller
     {
         \Log::info('Webhook received');
         
-
-        // $chat_id = $bot -> message() -> from -> id;
-        // $bot->sendMessage(chat_id: $chat_id, text: $response);
-
-
-        // $bot->onCommand('start', function (Nutgram $bot) {
-        //     $currentTime = date('H:i:s');
-        //     $response = "Привет! Текущее время: " . $currentTime;
-
-        //     $chat_id = $bot -> message() -> from -> id;
-        //     $bot->sendMessage(chat_id: $chat_id, text: $response);
-        // });
-
-       
-
-        // $bot->onCommand('start', function(Nutgram $bot){
-        //     $bot->sendMessage(
-        //         text: 'Do you want to know what`s time is it?',
-        //         reply_markup: ReplyKeyboardMarkup::make()->addRow(
-        //             KeyboardButton::make('What time is it?'),
-        //         )
-        //     );
-        // });
-
-    
-        // $bot->onText('What time is it?', function (Nutgram $bot) {
-        //     $currentTime = date('H:i:s');
-        //     $response = "Привет! Текущее время: " . $currentTime;
-        //     $chat_id = $bot -> message() -> from -> id;
-
-        //     $bot->sendMessage(chat_id: $chat_id, text: $response);
-        // });
-        
         $bot->onCommand('start', function(Nutgram $bot){
             $bot->sendMessage(
                 text: 'Hello!',
@@ -55,6 +22,7 @@ class TelegramController extends Controller
         });
         
         $bot->onText('Time', function (Nutgram $bot) {
+            date_default_timezone_set('Europe/Moscow');
             $currentTime = date('H:i:s');
             $response = 'Now is ' . $currentTime;;
             $chat_id = $bot -> message() -> from -> id;
