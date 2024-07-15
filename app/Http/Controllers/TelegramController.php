73dc90d6921e1b3c99f324408d289505740fa348
Anastasia <nassstya442@gmail.com>
2024-07-15 16:56:08 +0300

    Initial commit

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,10 +3,6 @@
 namespace App\Http\Controllers;
 use SergiX44\Nutgram\Nutgram;
 use Illuminate\Http\Request;
-use SergiX44\Nutgram\Telegram\Types\Keyboard\ReplyKeyboardMarkup;
-use SergiX44\Nutgram\Telegram\Types\Keyboard\KeyboardButton;
-<<<<<<< HEAD
-=======
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 
@@ -17,7 +13,6 @@ use App\Models\Product;
 // use App\Models\Message;
 // use App\Models\OrderHistory;
 // use App\Models\Customers;
->>>>>>> 21403cc (Initial commit)
 
 class TelegramController extends Controller
 {
@@ -26,29 +21,6 @@ class TelegramController extends Controller
         \Log::info('Webhook received');
         
         $bot->onCommand('start', function(Nutgram $bot){
-<<<<<<< HEAD
-            $bot->sendMessage(
-                text: 'Hello!',
-                reply_markup: ReplyKeyboardMarkup::make()->addRow(
-                    KeyboardButton::make('Time'),
-                )
-            );
-        });
-        
-        $bot->onText('Time', function (Nutgram $bot) {
-            date_default_timezone_set('Europe/Moscow');
-            $currentTime = date('H:i:s');
-            $response = 'Now is ' . $currentTime;;
-            $chat_id = $bot -> message() -> from -> id;
-
-            $bot->sendMessage(chat_id: $chat_id, text: $response);
-        });
-        
-        $bot->run();
-    }
-
-}
-=======
             $products = Product::all();
             $product = $products->first(); 
             $chat_id = $bot->message()->from->id;
@@ -85,82 +57,3 @@ class TelegramController extends Controller
         $bot->run();
     }
 }
-
-
-//  \Log::info('Webhook received');
-        
-//         $bot->onCommand('start', function(Nutgram $bot){
-//             $bot->sendMessage(
-//                 text: 'Hello!',
-//                 reply_markup: ReplyKeyboardMarkup::make()->addRow(
-//                     KeyboardButton::make('Time'),
-//                 )
-//             );
-//         });
-        
-//         $bot->onText('Time', function (Nutgram $bot) {
-//             date_default_timezone_set('Europe/Moscow');
-//             $currentTime = date('H:i:s');
-//             $response = 'Now is ' . $currentTime;;
-//             $chat_id = $bot -> message() -> from -> id;
-
-//             $bot->sendMessage(chat_id: $chat_id, text: $response);
-//         });
-        
-//         $bot->run();
-
-
-// $customers = Customers::all();
-        // $orders = OrderHistory::all();
-        // $message = Message::all();
-        
- // if ($products->isNotEmpty()) {
-            //     $product = $products->first(); 
-            //     $chat_id = $bot->message()->from->id;
-
-            //     $response = "Название: " . $product->title . "\n";
-            //     $response .= "Описание: " . $product->description . "\n";
-            //     $response .= "Цена: " . $product->price;
-
-            //     $bot->sendMessage(
-            //         chat_id: $chat_id,
-            //         text: $response,
-            //     );
-
-            // } else {
-            //     $text = "Продукт не найден.";
-            //     $chat_id = $bot->message()->from->id;
-            //     $bot->sendMessage(
-            //         chat_id: $chat_id,
-            //         text: $text,
-            //     );
-            // }
-
-            // if ($customers->isNotEmpty()) {
-            //     $customer = $customers->first(); 
-            //     $chat_id = $bot->message()->from->id;
-
-            //     $response = "ID покупателя: " . $customer->telegram_id . "\n";
-            //     $response .= "Покупатель: " . $customer->customer_name . "\n";
-
-            //     $bot->sendMessage(
-            //         chat_id: $chat_id,
-            //         text: $response,
-            //     );
-
-            // } else {
-            //     $text = "Покупатель не найден.";
-            //     $chat_id = $bot->message()->from->id;
-            //     $bot->sendMessage(
-            //         chat_id: $chat_id,
-            //         text: $text,
-            //     );
-            // }
-
-// $bot->onText('Buy', function (Nutgram $bot) {
-        //     $response = '"Buying"';
-        //     $chat_id = $bot -> message() -> from -> id;
-
-        //     $bot->sendMessage(chat_id: $chat_id, text: $response);
-        // });
->>>>>>> 21403cc (Initial commit)
--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TelegramController;
 use SergiX44\Nutgram\Nutgram;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\ProductController;
->>>>>>> 21403cc (Initial commit)
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-<<<<<<< HEAD
-Route::post('/webhook', [TelegramController::class, 'index']);
-
-=======
-// Route::post('/webhook', [TelegramController::class, 'index']);
-
-// Route::get('/product/title', [ProductController::class, 'showTitle']);
 
 Route::post('/webhook', [TelegramController::class, 'index']);
->>>>>>> 21403cc (Initial commit)
+
+
